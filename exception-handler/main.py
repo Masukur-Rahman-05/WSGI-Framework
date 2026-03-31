87cd52716e85669924fc30547de1795ccb1f7ca9
Masuk <masukrahoman5@gmail.com>
2026-03-31 17:08:01 +0600

    refactor: add a generic error handler

--- exception-handler/main.py
+++ exception-handler/main.py
@@ -1,5 +1,6 @@
 from wsgiref.simple_server import make_server
 import json
+from typing import Callable
 
 data = {
     "mobile":{
@@ -16,7 +17,7 @@ data = {
 
 
 
-def json_response(response : dict | list, status, start_response, response_header=None):
+def json_response(response : dict | list, status, start_response, response_header=None) -> list[bytes]:
 
     if response_header is None:
         response_header = []
@@ -25,23 +26,34 @@ def json_response(response : dict | list, status, start_response, response_heade
     start_response(status,response_header)
 
     return [response.encode('utf-8')]
+
+
+class Handler:
+    @staticmethod
+    def generic_exception_handler(environ,start_response, exception:Exception)->list[bytes]:
+        response_headers = [
+            ('Content-type','application/json')
+        ]
+            
+        response = {
+                "message":f"Unhandled Error has been occurred : {exception}"
+        }
+        return json_response(response=response,status='500 Internal server error',start_response=start_response,response_header=response_headers)
+
+
+
     
 class ExceptionHandler:
-    def __init__(self,app):
+    def __init__(self,app,exception_handler:Callable):
         self.wrapped_app = app
+        self.exception_handler = exception_handler
 
     def __call__(self,environ,start_response):
         try:
             return self.wrapped_app(environ,start_response)
         except Exception as e:
-            response_headers = [
-                ('Content-type','application/json')
-            ]
+            return self.exception_handler(environ,start_response,exception=e)
             
-            response = {
-                "message":f"Unhandled Error has been occurred : {e}"
-            }
-            return json_response(response=response,status='500 Internal server error',start_response=start_response,response_header=response_headers)
 
 
 def application(environ, start_response):
@@ -55,13 +67,21 @@ def application(environ, start_response):
         ('Content-type','application/json')
     ]
 
+    # Uncomment the next line to see the exception handling
+
     # raise RuntimeError('Error for testing')
     return json_response(response=product,status=status,start_response=start_response,response_header=response_headers)
 
 
-server = make_server('localhost',8000,app=ExceptionHandler(application))
-print('Server is listening on http://localhost:8000')
-server.serve_forever()
+if __name__ == "__main__":
+
+    wrapped_app = ExceptionHandler(
+        app = application,
+        exception_handler=Handler.generic_exception_handler
+    )
+    server = make_server('localhost',8000,app=wrapped_app)
+    print('Server is listening on http://localhost:8000')
+    server.serve_forever()
 
     
 
